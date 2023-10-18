f26e416e9c53ac757c049b859eb1fe707156c667
Robert336 <rmazza33@gmail.com>
2023-10-18 17:31:51 -0400

    get rid of the defailt page, begin working

---
+++ personal-website/src/app/components/ProjectCard.tsx
@@ -0,0 +1,13 @@
+import Pill from './Pill'
+
+export default function ProjectCard() {
+    return (
+      <>
+        <div className="grid-cols-3 gap-3 h-24 bg-slate-800 rounded p-2 flex">
+            <p>image here</p>
+            <p>text here</p>
+            <p>pill tags here</p>
+        </div>
+      </>
+    )
+}
