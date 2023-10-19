35b79e0373cec354efdbf5f600a9cf5eb6d8ad00
Robert336 <rmazza33@gmail.com>
2023-10-18 20:59:55 -0400

    more work on the project cards

--- personal-website/src/app/components/ProjectCard.tsx
+++ personal-website/src/app/components/ProjectCard.tsx
@@ -1,10 +1,12 @@
 import Pill from './Pill'
+import Image from 'next/image'
+import image_placeholder from '../../../public/image_placeholder.png'
 
 export default function ProjectCard() {
     return (
       <>
-        <div className="grid-cols-3 gap-3 h-24 bg-slate-800 rounded-lg p-2 flex shadow-inner-top">
-            <p>image here</p>
+        <div className="grid-cols-3 gap-3 h-24 w-full bg-slate-800 rounded-lg p-2 flex shadow-inner-top hover:bg-gradient-to-r from-cyan-500/[.15] to-purple-500/[.15]">
+            <Image src={image_placeholder} alt="preview screenshot of the project" className="max-w-[122px] rounded"/>
             <p>text here</p>
             <Pill text="TypeScript"/>
         </div>
--- personal-website/src/app/page.tsx
+++ personal-website/src/app/page.tsx
@@ -4,9 +4,28 @@ import ProjectCard from './components/ProjectCard'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24 bg-gray-900">
-      <h1 className="text-4xl font-bold text-sky-500">Robert Mazza</h1>
-      This is the home page
-      <ProjectCard/>
+      <h1 className="text-4xl font-bold text-sky-500 bg-gradient-to-r from-cyan-500 to-blue-500 text-transparent bg-clip-text pb-1">Robert Mazza</h1>
+      <div className="flex-row pb-1">
+        <a>links</a>
+        <a>go</a>
+        <a>here</a>
+      </div>
+      <h2 className="pb-10">
+        I&apos;m a Computer Science new graduate with a passion for building.
+      </h2>
+      <div>
+          <h3>1. About</h3>
+          <p>About text goes here</p>
+      </div>
+      <div className="flex flex-col gap-2 pt-10">
+        <h3>2. Projects</h3>
+        <ProjectCard/>
+        <ProjectCard/>
+        <ProjectCard/>
+        <ProjectCard/>
+      </div>
+
+      
     </main>
   )
 }
