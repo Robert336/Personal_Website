5f7ac900b7239de438b2889a1da91c584845c22a
Robert336 <rmazza33@gmail.com>
2023-10-22 18:52:47 -0400

    style links, divs, and sub title

--- personal-website/src/app/components/ProjectCard.tsx
+++ personal-website/src/app/components/ProjectCard.tsx
@@ -13,7 +13,7 @@ interface ProjectCardProps {
 export default function ProjectCard(props: ProjectCardProps) {
     return (
       <>
-        <div className="grid-cols-3 gap-3 h-24 max-w-2xl w-3/4 bg-slate-800 rounded-lg p-2 flex shadow-inner-top hover:bg-gradient-to-r from-cyan-500/[.15] to-purple-500/[.15]">
+        <div className="grid-cols-3 gap-3 max-w-2xl w-3/4 bg-slate-800 rounded-lg p-2 flex shadow-inner-top hover:bg-gradient-to-r from-cyan-500/[.15] to-purple-500/[.15]">
             <Image src={props.image ? props.image : image_placeholder} alt="preview screenshot of the project" className="max-w-[122px] rounded"/>
             <div className="flex flex-col">
               <p className="font-bold text-lg">{props.title}</p>
--- personal-website/src/app/page.tsx
+++ personal-website/src/app/page.tsx
@@ -3,21 +3,21 @@ import ProjectCard from './components/ProjectCard'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24 bg-gray-900">
+    <main className="flex min-h-screen flex-col items-center p-24 pt-10 bg-gray-900">
       <h1 className="text-4xl font-bold text-sky-500 bg-gradient-to-r from-cyan-500 to-blue-500 text-transparent bg-clip-text pb-1">Robert Mazza</h1>
-      <div className="flex-row pb-1">
-        <a>links</a>
-        <a>go</a>
-        <a>here</a>
+      <div className="flex flex-row pb-1 gap-3">
+        <a className="underline text-sky-500 hover:text-blue-800" href="https://github.com/Robert336">GitHub</a>
+        <a className="underline text-sky-500 hover:text-blue-800" href="https://www.linkedin.com/in/robert-mazza-6a8063203/">LinkedIn</a>
+        <a className="underline text-sky-500 hover:text-blue-800" href="rmazza33@gmail.com">rmazza33@gmail.com</a>
       </div>
-      <h2 className="pb-10">
+      <h2 className="pb-10 font-medium text-lg">
         I&apos;m a Computer Science new graduate with a passion for building solutions using tech.
       </h2>
-      <div>
+      <div className="flex flex-col self-start">
           <h3>1. About</h3>
           <p>About text goes here</p>
       </div>
-      <div className="flex flex-col items-center gap-2 pt-10 w-screen">
+      <div className="flex flex-col items-center gap-2 pt-10 w-full">
         <h3>2. Projects</h3>
         <ProjectCard title='Game Server Orchestration for Assetto Corsa' subtitle='sub0' tags={['JavaScript', 'Node.js', 'Express.js', 'Docker']} />
         <ProjectCard title='Reservation Automation Bot' subtitle='sub1' tags={['Python', 'Postman', 'Selenium']}/>
