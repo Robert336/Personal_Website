38a8ce7c6f34088062eb3916f3799f02a506f4a4
Robert336 <rmazza33@gmail.com>
2023-11-07 16:46:09 -0500

    updating ProjectCard image datatype

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -1,9 +1,9 @@
 import Pill from './Pill'
-import Image, { StaticImageData } from 'next/image'
+import Image from 'next/image'
 import image_placeholder from '../../../public/image_placeholder.png'
 
 interface ProjectCardProps {
-  image: StaticImageData,
+  image_src: string,
   title: string,
   subtitle: string,
   tags: Array<string>,
@@ -13,7 +13,7 @@ export default function ProjectCard(props: ProjectCardProps) {
     return (
       <>
         <div className="flex flex-col md:flex-row gap-3 max-w-full w-full md:max-w-2xl bg-slate-800 rounded-lg p-2 shadow-inner-top hover:bg-gradient-to-r from-cyan-500/[.15] to-purple-500/[.15]">
-            <Image src={props.image ? props.image : image_placeholder} alt="preview screenshot of the project" className="self-center w-full md:max-w-[150px] h-full rounded"/>
+            <Image src={props.image_src ? props.image_src : image_placeholder} alt="preview screenshot of the project" className="self-center w-full md:max-w-[150px] h-full rounded"/>
             <div className="flex flex-col">
               <h1 className="font-bold text-lg">{props.title}</h1>
               <p className="text-sm">{props.subtitle}</p>
