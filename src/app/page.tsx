18de756096fce2a14d44ce2620a034f3538bfe00
Robert336 <rmazza33@gmail.com>
2023-11-08 13:15:37 -0500

    fixing landing buttons responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,24 +7,28 @@ import CopyButton from './components/CopyButton'
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center min-h-screen bg-gray-900">
+    <main className="flex flex-col items-center min-h-screen max-w-screen bg-gray-900">
       <div className="flex flex-col items-center p-4 pt-10 max-w-2xl">
         <h1 className="text-4xl font-bold text-sky-500 bg-gradient-to-r from-cyan-500 to-blue-500 text-transparent bg-clip-text pb-5">Robert Mazza</h1>
         <h2 className="text-center pb-5">
           Hi, welcome to my personal website! 
           I&apos;m a Computer Science new graduate with a passion for building solutions using tech.
         </h2>
-        <div className="flex flex-row pb-5 gap-3">
+        <div className="flex flex-wrap justify-center pb-5 gap-3">
           <Button text='GitHub' href="https://github.com/Robert336"/>
           <Button text='LinkedIn' href="https://www.linkedin.com/in/robert-mazza-6a8063203/"/>
-          <CopyButton text='rmazza33@gmail.com' toCopy='rmazza33@gmail.com' />
+          {/* <CopyButton text='rmazza33@gmail.com' toCopy='rmazza33@gmail.com' /> */}
         </div>
         
         <div className="flex flex-col self-start">
           <h3 className="self-center text-xl font-bold text-cyan-500">
             About me
           </h3>
-          <p>Short about section goes here, prob not more than 1 paragraph. Short about section goes here, prob not more than 1 paragraph.Short about section goes here, prob not more than 1 paragraph.Short about section goes here, prob not more than 1 paragraph.Short about section goes here, prob not more than 1 paragraph.Short about section goes here, prob not more than 1 paragraph.Short about section goes here, prob not more than 1 paragraph.Short about section goes here, prob not more than 1 paragraph.</p>
+          <p>
+            I started coding in highschool and loved the capability of software to solve problems.
+            I've recently completed my BSc in Computer Science which gave me valuable knowledge
+            of operating systems, networking, 
+          </p>
         </div>
         <div className="flex flex-col items-center gap-2 pt-10 w-full">
           <h3 className="self-center text-xl font-bold text-cyan-500">
