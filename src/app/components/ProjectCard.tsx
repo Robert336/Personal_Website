500ec727d304ea64d4f459f7b65e70ae442abca3
Robert336 <rmazza33@gmail.com>
2023-11-13 12:42:25 -0500

    styling changes

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -12,12 +12,12 @@ interface ProjectCardProps {
 export default function ProjectCard(props: ProjectCardProps) {
     return (
       <>
-        <div className="flex flex-col md:flex-row gap-3 max-w-full w-full md:max-w-2xl bg-slate-800 rounded-lg p-2 shadow-inner-top hover:bg-gradient-to-r from-cyan-500/[.15] to-purple-500/[.15]">
-            <Image src={props.image_src ? props.image_src : image_placeholder} alt="preview screenshot of the project" className="self-center w-full md:max-w-[150px] h-full rounded"/>
+        <div className="flex flex-col md:flex-row gap-3 max-w-full w-full md:max-w-2xl bg-zinc-800 rounded-lg p-4 shadow-inner-top hover:bg-gradient-to-r from-cyan-500/[.15] to-purple-500/[.15]">
+            {/* <Image src={props.image_src ? props.image_src : image_placeholder} alt="preview screenshot of the project" className="self-center w-full md:max-w-[150px] h-full rounded"/> */}
             <div className="flex flex-col">
               <h1 className="font-bold text-lg">{props.title}</h1>
               <p className="text-sm">{props.subtitle}</p>
-              <div className="flex flex-wrap justify-start gap-1 pt-3 md:pt-1">
+              <div className="flex flex-wrap justify-start gap-1 pt-4 md:pt-2">
                 {props.tags.map((tag) => (
                   <Pill text={tag} key={tag}/> // using tag as the key because they are unique per project
                 ))}
